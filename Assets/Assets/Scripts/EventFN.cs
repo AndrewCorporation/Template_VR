4b7c99f63c20315f290a59ebbf6b59752120f35a
ReverseChin0 <38972126+ReverseChin0@users.noreply.github.com>
2021-04-26 21:08:46 -0500

    ClaseCampoVR
    moverse con Lerp
    Rotar
    Interactuar

--- Assets/Assets/Scripts/EventFN.cs
+++ Assets/Assets/Scripts/EventFN.cs
@@ -6,7 +6,8 @@ public class EventFN : MonoBehaviour
 {
 
     static public EventFN instance;
-    bool move = false;
+    static bool move = false;
+    static GameObject toClose;
 
     void Awake()
     {
@@ -22,6 +23,93 @@ public class EventFN : MonoBehaviour
                     Debug.Log("Sumar :: " + (int.Parse(_data[0] + int.Parse(_data[1]))));
                 }
                 break;
+            case "Click":
+                {
+                    Debug.Log("Click");
+                }
+                break;
+            case "Teleport":
+                {
+                    GameObject.Find("Pivote").transform.position = new Vector3(RaycasterObj.g_objToMove.transform.position.x, 1, RaycasterObj.g_objToMove.transform.position.z);
+                }
+                break;
+
+            case "Move":
+                {
+                    if (!move)
+                    {
+                        move = true;
+                        instance.StartCoroutine(c_move());
+                    }
+                }
+                break;
+
+            case "Interaccion":
+                {
+                    GameObject tmp = RaycasterObj.g_objToAct.transform.GetChild(0).gameObject;
+                    toClose = tmp;
+                    toClose.SetActive(true);
+                    toClose.transform.LookAt(GameObject.Find("Pivote").transform);
+                    toClose.transform.Rotate(Vector3.up,180);
+                }
+                break;
+            case "Rotar":
+                instance.StartCoroutine(c_rotatorObj(2));
+                break;
+
+            default:
+                {
+                    Debug.LogError("404 "+_function+" no existe");
+                }
+                break;
         }
+    }
+
+    static IEnumerator c_close()
+    {
+        yield return new WaitForSeconds(0.6f);
+        toClose.SetActive(false);
+    }
+
+    static IEnumerator c_rotatorObj(float _rotacion)
+    {
+        instance.StartCoroutine(c_close());
+
+        GameObject objtmp = RaycasterObj.g_objToAct;
+        float startRotacion = RaycasterObj.g_objToAct.transform.eulerAngles.y;
+        float endRotacion = startRotacion + 360.0f;
+        float t = 0.0f;
+        while(t < _rotacion)
+        {
+            t += Time.deltaTime;
+            float yRotacion = Mathf.Lerp(startRotacion, endRotacion, t / _rotacion) % 360.0f;
+            objtmp.transform.eulerAngles = new Vector3(objtmp.transform.eulerAngles.x, yRotacion, objtmp.transform.eulerAngles.z);
+            yield return null;
+        }
+        toClose.SetActive(true);
+    }
+
+    static IEnumerator c_move()
+    {
+        float elapsedTime = 0.0f;
+        float waitTime = 50.0f;
+
+        GameObject objTmp = GameObject.Find("Pivote");
+        Transform toMove = RaycasterObj.g_objToMove.transform;
+        Vector3 toUse = new Vector3(toMove.position.x, 1, toMove.position.z);
+
+        while(elapsedTime < waitTime)
+        {
+            objTmp.transform.position = Vector3.Lerp(objTmp.transform.position, toUse, elapsedTime / waitTime);
+            elapsedTime += Time.deltaTime;
+
+            if (Vector3.Distance(objTmp.transform.position, toUse) <= 0.1f)
+                break;
+            yield return null;
+        }
+
+        objTmp.transform.position = toUse;
+        yield return new WaitForSeconds(0.5f);
+        move = false;
     }
 }
---
+++ Assets/Assets/Scripts/RaycasterObj.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycasterObj : MonoBehaviour
+{
+    public static GameObject g_objToMove;
+    public static GameObject g_objToAct;
+
+    RaycastHit r_hit;
+
+    private void Update()
+    {
+        if(Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),out r_hit, Mathf.Infinity))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * r_hit.distance, Color.red);
+            if (r_hit.collider.gameObject.CompareTag("ToMove"))
+                g_objToMove = r_hit.collider.gameObject;
+
+            if (r_hit.collider.gameObject.CompareTag("ToAct"))
+                g_objToAct = r_hit.collider.gameObject;
+        }
+    }
+}
